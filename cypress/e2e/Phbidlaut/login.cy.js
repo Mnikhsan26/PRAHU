6a6ceb798140331ecace912d194590bf1bdf91d6
ikhsan26ph <ikhsan.prahu@gmail.com>
2025-03-06 09:24:04 +0700

    email & pass

--- cypress/e2e/Phbidlaut/login.cy.js
+++ cypress/e2e/Phbidlaut/login.cy.js
@@ -2,9 +2,9 @@ describe('phbidlaut login', () => {
   it('login_mandatoryfeild', () => {
     cy.visit('https://phbidlautdev.prahu-hub.com/user/login')
     cy.get('.heading_1').should('have.text','LOGIN')
-    cy.get('#email').type('phbiddaratadmean@gmail.com')
+    cy.get('#email').type('pengirim.ph2021@gmail.com')
     cy.get('#password').type('qwerty12345')
     cy.get('#submitLogin').click()
-    cy.get('.heading_1').should('have.text','AKUN SAYA')
+    cy.get('.heading_1').should('have.text','CARI PENAWARAN LELANG')
   })
 })
