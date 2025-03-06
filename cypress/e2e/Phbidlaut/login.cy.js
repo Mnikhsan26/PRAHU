7c8c8556bfda5abb671e192c0ededfbc75e062d8
ikhsan26ph <ikhsan.prahu@gmail.com>
2025-03-06 09:31:37 +0700

    email pass 2

--- cypress/e2e/Phbidlaut/login.cy.js
+++ cypress/e2e/Phbidlaut/login.cy.js
@@ -1,10 +1,11 @@
 describe('phbidlaut login', () => {
   it('login_mandatoryfeild', () => {
-    cy.visit('https://phbidlautdev.prahu-hub.com/user/login')
+    cy.visit('https://phbidlautdemo.prahu-hub.com/user/login')
     cy.get('.heading_1').should('have.text','LOGIN')
     cy.get('#email').type('pengirim.ph2021@gmail.com')
     cy.get('#password').type('qwerty12345')
     cy.get('#submitLogin').click()
-    cy.get('.heading_1').should('have.text','CARI PENAWARAN LELANG')
+    cy.get('.mb_20 > .col-12 > div > :nth-child(2)').should('have.text','Cari Penawaran Lelang')
+    
   })
 })
