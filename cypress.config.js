509ed70de4e41c510ede7922280a24a67cac213d
ikhsan <ikhsan.prahu@gmail.com>
2025-09-15 08:13:14 +0700

    update IK

--- cypress.config.js
+++ cypress.config.js
@@ -9,3 +9,18 @@ module.exports = defineConfig({
     specPattern: "cypress/e2e/**/*.cy.{js,jsx,ts,tsx}", // default pattern test
   },
 });
+
+module.exports = {
+  e2e: {
+    setupNodeEvents(on, config) {
+      on('before:browser:launch', (browser = {}, launchOptions) => {
+        if (browser.name === 'chrome') {
+          launchOptions.args.push('--disable-features=VizDisplayCompositor');
+          launchOptions.args.push('--disable-web-security');
+          launchOptions.args.push('--disable-site-isolation-trials');
+        }
+        return launchOptions;
+      });
+    }
+  }
+};
