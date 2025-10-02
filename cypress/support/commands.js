9a61d3640ea89a3ab4883264374de20922c57401
Mnikhsan26 <mnikhsan26@gmail.com>
2025-10-03 00:42:19 +0700

    Input Unit

--- cypress/support/commands.js
+++ cypress/support/commands.js
@@ -20,7 +20,7 @@
 // -- This will overwrite an existing command --
 // Cypress.Commands.overwrite('visit', (originalFn, url, options) => { ... })
 import NmrLelangPhbl from "../pages/nmrlelangphbl"
-
+import accountphbl from "../pages/accountphbl"
 
 Cypress.Commands.add('mockRecaptcha', () => {
   cy.window().then((win) => {
@@ -58,4 +58,34 @@ Cypress.Commands.add('isiNomorLelang', (tipe = 'normal', selector = '#nomor_lela
   Cypress.env('nomorLelang', nomor)
 
   // return nomor
+})
+
+// session bidowner
+Cypress.Commands.add('loginBidOwner', () => {
+  cy.visit('https://phbidlautdev.prahu-hub.com/user/login')
+  cy.get('.heading_1').should('have.text','LOGIN')
+  accountphbl.BidOwner()
+  accountphbl.PassAkun()
+  accountphbl.BtnLogin()
+  cy.url().should('not.include', '/login') // pastikan login berhasil
+})
+
+// session bidder
+Cypress.Commands.add('loginBidder', () => {
+  cy.visit('https://phbidlautdev.prahu-hub.com/user/login')
+  cy.get('.heading_1').should('have.text','LOGIN')
+  accountphbl.Bidder()
+  accountphbl.PassAkun()
+  accountphbl.BtnLogin()
+  cy.url().should('not.include', '/login') // pastikan login berhasil
+})
+
+// session admin
+Cypress.Commands.add('loginAdmin', () => {
+  cy.visit('https://phbidlautdev.prahu-hub.com/user/login')
+  cy.get('.heading_1').should('have.text','LOGIN')
+  accountphbl.Admin()
+  accountphbl.PassAdm()
+  accountphbl.BtnLogin()
+  cy.url().should('not.include', '/login') // pastikan login berhasil
 })
