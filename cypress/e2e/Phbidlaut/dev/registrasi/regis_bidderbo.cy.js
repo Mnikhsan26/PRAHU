74dd025c6cb5e9a3815b8958774d1a988479ff8b
ikhsan <ikhsan.prahu@gmail.com>
2025-10-15 08:59:11 +0700

    Login

--- cypress/e2e/Phbidlaut/dev/registrasi/regis_bidderbo.cy.js
+++ cypress/e2e/Phbidlaut/dev/registrasi/regis_bidderbo.cy.js
@@ -2,7 +2,7 @@ import RegisterPage from "../../../../pages/regisphbl"
 
 describe('case_registrasi', () => {
 
-  it('case_alert_radiobutton', () => {
+  it.skip('case_alert_radiobutton', () => {
     cy.visit('https://phbidlautdev.prahu-hub.com/user/login')        
     cy.get('.heading_1').should('have.text','LOGIN')
     cy.get('.col-md-7 > .link_1').click()
@@ -11,7 +11,7 @@ describe('case_registrasi', () => {
     cy.get('.popover-body').should('contain.text', 'Pilih Jenis User')
   })
 
-  it('case_alert_nama', () => {
+  it.skip('case_alert_nama', () => {
     cy.visit('https://phbidlautdev.prahu-hub.com/user/login')        
     cy.get('.heading_1').should('have.text','LOGIN')
     cy.get('.col-md-7 > .link_1').click()
@@ -21,7 +21,7 @@ describe('case_registrasi', () => {
     cy.get('.popover-body').should('contain.text', 'Masukkan Nama')
   })
 
-  it('case_alert_notlp', () => {
+  it.skip('case_alert_notlp', () => {
     cy.visit('https://phbidlautdev.prahu-hub.com/user/login')        
     cy.get('.heading_1').should('have.text','LOGIN')
     cy.get('.col-md-7 > .link_1').click()
@@ -32,7 +32,7 @@ describe('case_registrasi', () => {
     cy.get('.popover-body').should('contain.text', 'Masukkan No. Telepon')
   })
 
-  it('case_alert_notlpsama', () => {
+  it.skip('case_alert_notlpsama', () => {
     const page = new RegisterPage()
 
     cy.visit('https://phbidlautdev.prahu-hub.com/user/login')        
@@ -46,7 +46,7 @@ describe('case_registrasi', () => {
     cy.get('.popover-body').should('contain.text', 'Nomor whatsapp sudah terdaftar di sistem')
   })
 
-  it('case_alert_namaperusahaan', () => {
+  it.skip('case_alert_namaperusahaan', () => {
     const page = new RegisterPage()
 
     cy.visit('https://phbidlautdev.prahu-hub.com/user/login')        
@@ -60,7 +60,7 @@ describe('case_registrasi', () => {
     cy.get('.popover-body').should('contain.text', 'Masukkan Nama Perusahaan')
   })
 
-  it('case_alert_email', () => {
+  it.skip('case_alert_email', () => {
     const page = new RegisterPage()
 
     cy.visit('https://phbidlautdev.prahu-hub.com/user/login')        
@@ -75,7 +75,7 @@ describe('case_registrasi', () => {
     cy.get('.popover-body').should('contain.text', 'Masukkan Email')
   })
 
-  it('case_alert_emailsama', () => {
+  it.skip('case_alert_emailsama', () => {
     const page = new RegisterPage()
 
     cy.visit('https://phbidlautdev.prahu-hub.com/user/login')        
@@ -91,7 +91,7 @@ describe('case_registrasi', () => {
     cy.get('.popover-body').should('contain.text', 'Email sudah terdaftar di sistem')
   })
 
-  it('case_alert_emailtdkvalid', () => {
+  it.skip('case_alert_emailtdkvalid', () => {
     const page = new RegisterPage()
 
     cy.visit('https://phbidlautdev.prahu-hub.com/user/login')        
@@ -109,7 +109,7 @@ describe('case_registrasi', () => {
     cy.get('.popover-body').should('contain.text', 'Masukkan Email Dengan Benar')
   })
 
-  it('case_alert_katasandi', () => {
+  it.skip('case_alert_katasandi', () => {
     const page = new RegisterPage()
 
     cy.visit('https://phbidlautdev.prahu-hub.com/user/login')        
@@ -125,7 +125,7 @@ describe('case_registrasi', () => {
     cy.get('.popover-body').should('contain.text', 'Masukkan Kata Sandi')
   })
 
-  it('case_alert_sandikrg6digit', () => {
+  it.skip('case_alert_sandikrg6digit', () => {
     const page = new RegisterPage()
 
     cy.visit('https://phbidlautdev.prahu-hub.com/user/login')        
@@ -142,7 +142,7 @@ describe('case_registrasi', () => {
     cy.get('.popover-body').should('contain.text', 'Kata Sandi Minimal 6 Digit')
   })
 
-  it('case_alert_sandikmbinasi', () => {
+  it.skip('case_alert_sandikmbinasi', () => {
     const page = new RegisterPage()
 
     cy.visit('https://phbidlautdev.prahu-hub.com/user/login')        
@@ -159,7 +159,7 @@ describe('case_registrasi', () => {
     cy.get('.popover-body').should('contain.text', 'Kombinasi Hanya Boleh Huruf dan Angka')
   })
 
-  it('case_alert_ketikulangkatasandi', () => {
+  it.skip('case_alert_ketikulangkatasandi', () => {
     const page = new RegisterPage()
 
     cy.visit('https://phbidlautdev.prahu-hub.com/user/login')        
@@ -176,7 +176,7 @@ describe('case_registrasi', () => {
     cy.get('.popover-body').should('contain.text', 'Masukkan Ulang Kata Sandi')
   })
 
-  it('case_alert_katasanditidakcocok', () => {
+  it.skip('case_alert_katasanditidakcocok', () => {
     const page = new RegisterPage()
 
     cy.visit('https://phbidlautdev.prahu-hub.com/user/login')        
@@ -194,7 +194,7 @@ describe('case_registrasi', () => {
     cy.get('.popover-body').should('contain.text', 'Kata Sandi Tidak Cocok')
   })
 
-  it('registrasi_berhasil', () => {
+  it.skip('registrasibo_berhasil', () => {
     const page = new RegisterPage()
 
     cy.visit('https://phbidlautdev.prahu-hub.com/user/login')        
@@ -207,6 +207,66 @@ describe('case_registrasi', () => {
     const phone = page.inputPhone()
     cy.get('#nama_perusahaan').type('PT. Jaya Muda Abadi')
     const email = page.inputEmail('bidowner')
+
+    cy.get('#password').type('qwerty12345')
+    cy.get('#password_ulang').type('qwerty12345')
+    cy.get('#submitRegistrasi').click()
+    
+  })
+
+  it.skip('registrasibidder_berhasil', () => {
+    const page = new RegisterPage()
+
+    cy.visit('https://phbidlautdev.prahu-hub.com/user/login')        
+    cy.get('.heading_1').should('have.text','LOGIN')
+    cy.get('.col-md-7 > .link_1').click()
+    cy.get('.heading_1').should('have.text','REGISTRASI')
+    cy.get('#radioBidder').click()
+    cy.get('#nama').type('Jayandaru Kintamani')
+
+    const phone = page.inputPhone()
+    cy.get('#nama_perusahaan').type('PT. Transportasi Jaya Umum')
+    const email = page.inputEmail('bidder')
+
+    cy.get('#password').type('qwerty12345')
+    cy.get('#password_ulang').type('qwerty12345')
+    cy.get('#submitRegistrasi').click()
+    
+  })
+
+  it('registrasibo_kirimulang', () => {
+    const page = new RegisterPage()
+
+    cy.visit('https://phbidlautdev.prahu-hub.com/user/login')        
+    cy.get('.heading_1').should('have.text','LOGIN')
+    cy.get('.col-md-7 > .link_1').click()
+    cy.get('.heading_1').should('have.text','REGISTRASI')
+    cy.get('#radioBidOwner').click()
+    cy.get('#nama').type('Jayandaru Kintamani')
+
+    const phone = page.inputPhone()
+    cy.get('#nama_perusahaan').type('PT. Jaya Muda Abadi')
+    const email = page.inputEmail('bidowner')
+
+    cy.get('#password').type('qwerty12345')
+    cy.get('#password_ulang').type('qwerty12345')
+    cy.get('#submitRegistrasi').click()
+    
+  })
+
+  it('registrasibidder_kirimulang', () => {
+    const page = new RegisterPage()
+
+    cy.visit('https://phbidlautdev.prahu-hub.com/user/login')        
+    cy.get('.heading_1').should('have.text','LOGIN')
+    cy.get('.col-md-7 > .link_1').click()
+    cy.get('.heading_1').should('have.text','REGISTRASI')
+    cy.get('#radioBidder').click()
+    cy.get('#nama').type('Jayandaru Kintamani')
+
+    const phone = page.inputPhone()
+    cy.get('#nama_perusahaan').type('PT. Transportasi Jaya Umum')
+    const email = page.inputEmail('bidder')
 
     cy.get('#password').type('qwerty12345')
     cy.get('#password_ulang').type('qwerty12345')
